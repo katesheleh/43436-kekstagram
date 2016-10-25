5a9ba9ad7db234a36074ee1d25dc167215af3395
Kate Sheleh <kate.sheleh@gmail.com>
2016-10-25 11:36:26 +0200

    скрипт pictures.js разбит на три модуля

---
+++ src/js/render.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var container = document.querySelector('.pictures');
+
+var renderPictures = function(pictures) {
+  var filters = document.querySelector('.filters');
+  filters.classList.add('hidden');
+
+  pictures.forEach(function(picture) {
+    container.appendChild(getPictureElement(picture));
+  })
+
+  filters.classList.remove('hidden');
+};
