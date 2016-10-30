0590a4596810e666625a07fddaed0f2434eb38e3
Kate Sheleh <kate.sheleh@gmail.com>
2016-10-30 17:34:15 +0100

    открываются и закрываются фото

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -12,8 +12,8 @@ function Gallery() {
   var pictures = [];
   var activePicture = 0;
 
-  this.data = data;
-  this.element = getPictureElement(data);
+  // this.data = data;
+  // this.element = getPictureElement(data);
 
   var self = this;
 
@@ -21,29 +21,27 @@ function Gallery() {
     var image = imgElement;
   };
 
-  var hide = function() {
-    closeElement.addEventListener('click', function(event) {
-      event.preventDefault();
-      galleryContainer.classList.add('invisible');
-    });
-  }();
+  var hide = function(event) {
+    event.preventDefault();
+    galleryContainer.classList.add('invisible');
+  }
+
+  closeElement.addEventListener('click', hide);
 
   var show = function(pics) {
-
-    fotoBlock.addEventListener('click', function(event) {
-      event.preventDefault();
-      // Этот вызов удаляет указанный класс у элемента с классом gallery-overlay
-      galleryContainer.classList.remove('invisible');
-    });
-
+    event.preventDefault();
+    galleryContainer.classList.remove('invisible');
 
     pictures.forEach(function(pic) {
       // да, сюда нужно каким-то образом записать данные....
-      // загруженные демо, особенно "Фотогалерея Кексобукинга", ужасны. Без объяснения где и что берется и т.д. Информация очень неудобно подана.
+      // загруженные демо, особенно "Фотогалерея Кексобукинга", ужасны. Без объяснения где и что берется и т.д.
     });
+
     activePicture = 0;
     setActivePicture(activePicture);
-  }();
+  };
+
+  fotoBlock.addEventListener('click', show);
 }
 
 // проверку не проходит из-за ряда неиспользованных значений, которые еще будут доработаны, если будут конечно доработаны..
