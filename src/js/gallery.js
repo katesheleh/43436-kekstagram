dee5e30584857fb6d08d9bc544706362af1072d9
Kate Sheleh <kate.sheleh@gmail.com>
2016-10-30 17:01:25 +0100

    комментарии к замечаниям

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -1,5 +1,7 @@
 'use strict';
 
+var getPictureElement = require('./picture.js');
+
 function Gallery() {
   var galleryContainer = document.querySelector('.gallery-overlay');
   var closeElement = galleryContainer.querySelector('.gallery-overlay-close');
@@ -10,10 +12,13 @@ function Gallery() {
   var pictures = [];
   var activePicture = 0;
 
-  var setActivePicture = function(picture) {
-    var image = galleryContainer.querySelectorAll('img');
+  this.data = data;
+  this.element = getPictureElement(data);
 
-    // preview.src = image[picture].src;
+  var self = this;
+
+  var setActivePicture = function(picture) {
+    var image = imgElement;
   };
 
   var hide = function() {
@@ -27,21 +32,24 @@ function Gallery() {
 
     fotoBlock.addEventListener('click', function(event) {
       event.preventDefault();
+      // Этот вызов удаляет указанный класс у элемента с классом gallery-overlay
       galleryContainer.classList.remove('invisible');
     });
 
-    pictures.forEach(function(pic) {
 
+    pictures.forEach(function(pic) {
+      // да, сюда нужно каким-то образом записать данные....
+      // загруженные демо, особенно "Фотогалерея Кексобукинга", ужасны. Без объяснения где и что берется и т.д. Информация очень неудобно подана.
     });
     activePicture = 0;
     setActivePicture(activePicture);
   }();
 }
 
+// проверку не проходит из-за ряда неиспользованных значений, которые еще будут доработаны, если будут конечно доработаны..
 var imgGallery = new Gallery();
 
 console.log(imgGallery);
-
 
 
 module.exports = Gallery;
